8df7401b5f2b606f834a17990a66bc7fc526b2d3
Cristopher <31778194+Urfenox@users.noreply.github.com>
2025-11-01 23:03:34 -0300

    If UV is available, use it when running the server

--- START-SERVER.ps1
+++ START-SERVER.ps1
@@ -1,4 +1,10 @@
-# Carga el entorno virtual de Python
-.\.venv\Scripts\activate
-# Ejecuta el script principal de SocketeDeck
-py SocketeDeck.py
+﻿# Si 'uv' esta disponible, se usa.
+if (Test-Path -Path "uv.lock" -PathType Leaf) {
+    # Ejecuta el script principal de SocketeDeck
+    uv run SocketeDeck.py
+} else {
+    # Carga el entorno virtual de Python
+    .\.venv\Scripts\activate
+    # Ejecuta el script principal de SocketeDeck
+    py SocketeDeck.py
+}
