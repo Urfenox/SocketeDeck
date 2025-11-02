42c6436ba1437b9c3d1a05cc7e421e15a0339a42
Cristopher <31778194+Urfenox@users.noreply.github.com>
2025-11-01 23:06:59 -0300

    Update START-SERVER.ps1

--- START-SERVER.ps1
+++ START-SERVER.ps1
@@ -1,8 +1,10 @@
 ﻿# Si 'uv' esta disponible, se usa.
 if (Test-Path -Path "uv.lock" -PathType Leaf) {
+    Write-Host "Usando uv para iniciar el servidor..."; Sleep 1
     # Ejecuta el script principal de SocketeDeck
     uv run SocketeDeck.py
 } else {
+    Write-Host "Usando pyenv para iniciar el servidor..."; Sleep 1
     # Carga el entorno virtual de Python
     .\.venv\Scripts\activate
     # Ejecuta el script principal de SocketeDeck
